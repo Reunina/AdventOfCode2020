173bcc8046daea01b39a0624035353046dc2445e
sara <sara.tahabik@gmail.com>
2020-12-11 08:31:04 +0100

    Day 01 - Part 02

--- src/main/kotlin/day01/Entry.kt
+++ src/main/kotlin/day01/Entry.kt
@@ -2,31 +2,55 @@ package day01
 
 import java.io.File
 
-class EntryCode(val firstEntry: Int, val secondEntry: Int) {
+class EntryCode(val firstEntry: Int, val secondEntry: Int, val thirdEntry: Int) {
 
-    private val code: Int = firstEntry * secondEntry
+    private val code: Int = firstEntry * secondEntry * thirdEntry
 
 
-    constructor() : this(0, 0)
+    constructor() : this(0, 0,0)
+    constructor(firstEntry: Int, secondEntry: Int) : this(firstEntry,secondEntry,1)
 
-    private fun findEntriesThatSumUpTo2020(input: List<Int>): Set<Int> {
-        return input.associateWith { 2020 - it }.filterValues { input.contains(it) }.keys
+    private val EXPECTED_SUM = 2020
+
+    private fun findTwoEntriesThatSumUpTo2020(input: List<Int>): Set<Int> {
+        return input.associateWith { EXPECTED_SUM - it }.filterValues { input.contains(it) }.keys
     }
 
-    fun findFrom(input: List<Int>): EntryCode {
-        val entries = findEntriesThatSumUpTo2020(input)
+    private fun findThreeEntriesThatSumUpTo2020(input: List<Int>): Set<Int> {
+        val values = input
+                .map { it to findTwoOtherEntriesThatSumUpTo2020With(it, input) }
+                .toMap()
+                .filterNot { it.value.isNullOrEmpty() }
+                .entries
+                .first()
+                .toPair()
+        return setOf(values.first) union values.second
+    }
+
+    private fun findTwoOtherEntriesThatSumUpTo2020With(firstEntry: Int, input: List<Int>): Set<Int> {
+        return input.associateWith { EXPECTED_SUM - (firstEntry + it)  }
+                .filterValues { input.contains(it) }
+                .keys
+    }
+
+    fun findFromTwoEntriesOf(input: List<Int>): EntryCode {
+        val entries = findTwoEntriesThatSumUpTo2020(input)
         return EntryCode(entries.first(), entries.last())
+    }
+
+    fun findFromThreeEntriesOf(input: List<Int>): EntryCode {
+        val entries = findThreeEntriesThatSumUpTo2020(input)
+        return EntryCode(entries.first(), entries.elementAt(1), entries.last())
     }
 
     override fun toString(): String {
         return "$firstEntry + $secondEntry = $code"
     }
-
-
 }
 
 
 fun main(args: Array<String>) {
     val input: List<Int> = File("src/main/resources/day01/puzzle_input_for_entry.txt").readLines().map(String::toInt)
-    println("Code to enter Advent Of Code 2020: " + EntryCode().findFrom(input))
+    println("Code to enter Advent Of Code 2020:       " + EntryCode().findFromTwoEntriesOf(input))
+    println("Code to continue on Advent Of Code 2020: " + EntryCode().findFromThreeEntriesOf(input))
 }
--- src/test/kotlin/day01/EntryTest.kt
+++ src/test/kotlin/day01/EntryTest.kt
@@ -7,10 +7,19 @@ import org.junit.jupiter.api.Test
 internal class EntryTest {
 
     @Test
-    fun shouldFindTeEntryCode() {
+    fun shouldFindTheEntryCode() {
         val input = listOf(1721, 979, 366, 299, 675, 1456)
-        assertThat(EntryCode().findFrom(input)).isEqualToComparingFieldByField(EntryCode(1721, 299))
+        assertThat(EntryCode().findFromTwoEntriesOf(input))
+                .isEqualToComparingFieldByField(EntryCode(1721, 299))
+                .hasFieldOrPropertyWithValue("code", 514579)
+    }
+
+    @Test
+    fun shouldFindTheContinuationCode() {
+        val input = listOf(1721, 979, 366, 299, 675, 1456)
+        assertThat(EntryCode().findFromThreeEntriesOf(input))
+                .isEqualToComparingFieldByField(EntryCode(979, 366, 675))
+                .hasFieldOrPropertyWithValue("code", 241861950)
     }
 
 }
-
