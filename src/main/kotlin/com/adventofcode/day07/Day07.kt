c4303b958eb9b223235029ff03a33080095fe099
sara <sara.tahabik@gmail.com>
2020-12-20 15:57:10 +0100

    Init Day 07

---
+++ src/main/kotlin/com/adventofcode/day07/Day07.kt
@@ -0,0 +1,12 @@
+package com.adventofcode.day07
+
+import java.io.File
+ 
+fun main() {
+    val input: List<Int> =
+            File("src/main/resources/day07/puzzle_input.txt")
+                    .readLines()
+                    .map(String::toInt)
+    println("No code yet for this Day ! ")
+}
+ 
