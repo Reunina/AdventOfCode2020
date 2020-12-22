c52c8190cc61247ec88f454ce0b3959895eabccb
sara <sara.tahabik@gmail.com>
2020-12-22 20:31:12 +0100

    Day 09 -- Part 02

--- src/main/kotlin/com/adventofcode/day09/XmasCipherDecoder.kt
+++ src/main/kotlin/com/adventofcode/day09/XmasCipherDecoder.kt
@@ -1,14 +1,18 @@
 package com.adventofcode.day09
 
+import java.util.*
+
 typealias Code = Long
 
-class XmasCipherDecoder(val preambleAndCodes: Collection<Code>, val preambleSize : Int) {
+class XmasCipherDecoder(private val preambleAndCodes: Collection<Code>, private val preambleSize: Int) {
 
-    var preamble = preambleAndCodes.toMutableList().subList(0, preambleSize)
-    val codes = preambleAndCodes.toList().subList( preambleSize ,preambleAndCodes.size )
+    private val initialPreamble = preambleAndCodes.toMutableList().subList(0, preambleSize)
+    private val codes = preambleAndCodes.toList().subList(preambleSize, preambleAndCodes.size)
 
-    constructor( preamble: Collection<Code>,   codes: Collection<Code> = mutableListOf()) :
-            this(preamble + codes , preamble.size)
+    var preamble = initialPreamble
+
+    constructor(preamble: Collection<Code>, codes: Collection<Code> = mutableListOf()) :
+            this(preamble + codes, preamble.size)
 
 
     constructor(preambleAsInts: List<Int>) : this(preambleAsInts.map { it.toLong() }.toList())
@@ -16,16 +20,15 @@ class XmasCipherDecoder(val preambleAndCodes: Collection<Code>, val preambleSize
     companion object {
 
         fun readFrom(input: String, preambleLength: Int): XmasCipherDecoder {
-            val data = input.split('\n').filterNot{it.isEmpty()}.map { it.toLong() }
+            val data = input.split('\n').filterNot { it.isEmpty() }.map { it.toLong() }
             val preamble = data.subList(0, preambleLength)
-            val codes = data.subList(preambleLength , data.size)
+            val codes = data.subList(preambleLength, data.size)
             return XmasCipherDecoder(preamble, codes)
         }
-
     }
 
     fun validate(expected: Int): Boolean {
-        return validate (expected.toLong())
+        return validate(expected.toLong())
     }
 
 
@@ -40,6 +43,7 @@ class XmasCipherDecoder(val preambleAndCodes: Collection<Code>, val preambleSize
     }
 
     fun findFirstInvalidCode(): Code? {
+        preamble = initialPreamble
         codes.forEach {
             val validity = validate(it)
             if (!validity) return it
@@ -48,8 +52,24 @@ class XmasCipherDecoder(val preambleAndCodes: Collection<Code>, val preambleSize
         return null
     }
 
-    fun findEncryptionWeakness(): Code {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun findEncryptionWeakness(): Long {
+        val invalidCode = findFirstInvalidCode()
+
+        val values = preambleAndCodes
+                .map { generateAllSequencesForCode(it) }
+                .first { it.containsKey(invalidCode) }[invalidCode]
+
+        return values!!.first!! + values.second!!.toLong()
+    }
+
+    private fun generateAllSequencesForCode(departureCode: Code): Map<Long, Pair<Code?, Code?>> {
+        val maxsize = preambleAndCodes.size
+        val fromIndex = preambleAndCodes.indexOf(departureCode)
+        return (preambleAndCodes as ArrayList).subList(fromIndex, maxsize)
+                .mapIndexed { index, _ -> preambleAndCodes.subList(fromIndex, maxsize - index) }
+                .filter { it.size > 1 }
+                .map { it.sum() to Pair(it.min(), it.max()) }
+                .toMap()
     }
 
 
